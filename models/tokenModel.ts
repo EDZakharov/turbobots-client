907c062b6247ec0ae71de95d49f0f3252bfaa9df
Evgeniy <81650829+EDZakharov@users.noreply.github.com>
2024-03-09 19:36:09 +0300

    Delete models directory

--- models/tokenModel.ts
+++
@@ -1,8 +0,0 @@
-import { InferSchemaType, Schema, model } from 'mongoose'
-
-const TokenSchema = new Schema({
-	userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
-	refreshToken: { type: String, required: true },
-})
-export type UserToken = InferSchemaType<typeof TokenSchema>
-export const TokenModel = model('tokens', TokenSchema)
